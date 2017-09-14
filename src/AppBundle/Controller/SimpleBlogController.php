a8d774b6d2eb7117703537a29e98cff58067aa41
Tomislav Strugačevac <tomislav.strugacevac@gmail.com>
2017-09-14 14:45:37 +0200

    Added form for creating new blog entry

--- src/AppBundle/Controller/SimpleBlogController.php
+++ src/AppBundle/Controller/SimpleBlogController.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Blog;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,7 +31,57 @@ class SimpleBlogController extends Controller
      */
     public function createAction(Request $request)
     {
-        return $this->render('blog/create.html.twig');
+        $blog = new Blog;
+        $form = $this->createFormBuilder($blog)
+        -> add( 'title', TextType::class, [
+                            'attr' => [
+                                'class' => 'form-control',
+                                'style' => 'margin-bottom:15px'
+           ]])
+        -> add( 'body', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px'
+                ]])
+        -> add( 'createdBy', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px'
+                ]])
+        -> add( 'submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary btn.md',
+                    'label' => 'Submit New Blog Entry'
+                ]])
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid() ){
+            $title = $form['title']->getData();
+            $body = $form['body']->getData();
+            $createdBy = $form['createdBy']->getData();
+            $timeCreated = new\DateTime('now');
+
+            $blog->setTitle($title);
+            $blog->setBody($body);
+            $blog->setCreatedBy($createdBy);
+            $blog->setTimeCreated($timeCreated);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blog);
+            $em->flush();
+
+            $this->addFlash(
+                'notice', 'New Blog Added'
+            );
+
+            return $this->redirectToRoute('blog_index');
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
